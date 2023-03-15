b9da247e6a967e4f4ad830f899cd4f1d927ec656
AirMouse <airmouselol@gmail.com>
2023-03-15 16:05:17 +0300

    refactor: ui рефакторинг

--- src/features/addTodoItem/components.ts
+++ src/features/addTodoItem/components.ts
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
   margin-bottom: 50px;
   display: flex;
+  margin-left: auto;
+  margin-right: auto;
 
   input {
     min-width: 270px;
